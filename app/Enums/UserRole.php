177d00179b77a4dd591f1adbe8b3c44ecd6505dd
TadejDragojlovic <dragojlovic.tadej@gmail.com>
2026-09-03 19:18:26 +0200

    dodaj kontrolnu tablu prilagodjenu ulozi

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -7,4 +7,13 @@ enum UserRole: string
     case KUPAC = 'kupac';
     case ZAPOSLENI = 'zaposleni';
     case ADMIN = 'admin';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::KUPAC => 'Kupac',
+            self::ZAPOSLENI => 'Zaposleni',
+            self::ADMIN => 'Administrator',
+        };
+    }
 }
